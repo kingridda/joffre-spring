dab57690a92af057324bde2bfcfe8d9f4380e5cf
kingridda <abdelghaniridda01@gmail.com>
2021-06-08 19:36:14 +0000

    updating index

--- src/main/java/com/joffre/joffrespring/controllers/Index.java
+++ src/main/java/com/joffre/joffrespring/controllers/Index.java
@@ -1,12 +1,16 @@
 package com.joffre.joffrespring.controllers;
 
+import com.joffre.joffrespring.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index {
     @RequestMapping(value = "/")
-    public String index(){
+    public String index(Model model){
+        User user = new User();
+        model.addAttribute("user", user);
         return "index";
     }
 }
