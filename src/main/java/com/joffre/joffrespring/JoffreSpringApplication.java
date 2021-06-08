85284871e8ac25336a9e681be297266c2a302794
kingridda <abdelghaniridda01@gmail.com>
2021-06-08 16:23:31 +0000

    first commit

---
+++ src/main/java/com/joffre/joffrespring/JoffreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.joffre.joffrespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoffreSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoffreSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joffre/joffrespring/JoffreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joffre.joffrespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoffreSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
