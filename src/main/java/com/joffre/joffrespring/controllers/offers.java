e9bf38ea1115c6f37aa0e90ba8417e583d1bf19b
Anas-Abou-Ali <anasabouali99@gmail.com>
2021-06-08 21:03:56 +0100

    offers not finished yet

---
+++ src/main/java/com/joffre/joffrespring/controllers/offers.java
@@ -0,0 +1,18 @@
+package com.joffre.joffrespring.controllers;
+
+import com.joffre.joffrespring.models.Offre;
+import com.joffre.joffrespring.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class offers {
+    @RequestMapping(value = "/offers")
+    public String offers(Model model){
+        Offre offre = new Offre();
+
+        model.addAttribute("offers", offre);
+        return "offers";
+    }
+}
