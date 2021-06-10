43961124eda9af23b2b515a0ec7974bf38acf7d8
kingridda <abdelghaniridda01@gmail.com>
2021-06-10 07:24:38 +0000

    -

--- src/main/java/com/joffre/joffrespring/controllers/profile.java
+++ src/main/java/com/joffre/joffrespring/controllers/profile.java
@@ -3,12 +3,16 @@ package com.joffre.joffrespring.controllers;
 import com.joffre.joffrespring.entities.Message;
 import com.joffre.joffrespring.entities.Offre;
 import com.joffre.joffrespring.entities.User;
+import com.joffre.joffrespring.services.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.joffre.joffrespring.util.SharedEnums.Category;
 import static com.joffre.joffrespring.util.SharedEnums.City;
@@ -20,16 +24,18 @@ public class profile {
 
     @Autowired
     private HttpSession session;
+    @Autowired
+    private OfferService offerService;
 
     @RequestMapping(value = "/profile")
     public String offer(Model model){
 
         User user = (User) session.getAttribute(USER_SESSION);
         Offre offre = new Offre();
-        Message mess = new Message();
+        //user.setOffers(offerService.list());
         model.addAttribute("user", user);
         model.addAttribute("offre", offre);
-        model.addAttribute("mess",mess);
+        model.addAttribute("mess",new Message());
         return "profile";
     }
 }
