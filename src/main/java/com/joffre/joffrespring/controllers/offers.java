0f4c4e3616e5031296336de3164f7caaf519caf1
kingridda <abdelghaniridda01@gmail.com>
2021-06-10 03:55:46 +0000

    offers service and detail html updates

--- src/main/java/com/joffre/joffrespring/controllers/offers.java
+++ src/main/java/com/joffre/joffrespring/controllers/offers.java
@@ -1,12 +1,15 @@
 package com.joffre.joffrespring.controllers;
 
 import com.joffre.joffrespring.entities.Offre;
+import com.joffre.joffrespring.entities.User;
 import com.joffre.joffrespring.services.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 import static com.joffre.joffrespring.util.SharedEnums.Category;
 import static com.joffre.joffrespring.util.SharedEnums.City;
@@ -17,6 +20,9 @@ public class offers {
 
     @Autowired
     private OfferService offerService;
+
+    @Autowired
+    private HttpSession session;
 
     @RequestMapping(value = "/offers")
     public String offers(Model model){
@@ -31,8 +37,12 @@ public class offers {
     @RequestMapping(value = "/offers/{offerId}")
     public String offer(@PathVariable long offerId , Model model){
 
+        User user = (User) session.getAttribute("user");
+        Offre offer = offerService.getById(offerId);
+        offer.setUser(user);
 
-        model.addAttribute("offer", new Offre());
+
+        model.addAttribute("offer", offer );
         model.addAttribute("City", City);
         model.addAttribute("Category", Category);
         return "offerDetail";
